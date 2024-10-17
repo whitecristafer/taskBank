1aee824b106c2eadd7a872ba27a01a03fa69a64a
Кот Андрей Ильич <kot.ai@att.edu.ru>
2024-10-17 16:04:04 +0300

    Made a Card object

--- src/main/java/org/example/app/App.java
+++ src/main/java/org/example/app/App.java
@@ -1,7 +1,10 @@
 package org.example.app;
 
+import org.example.bank.Card;
+
 public class App {
     public static void main(String[] args) {
-
+    Card card = new Card(123543241233123L);
+    card.showCardInfo();
     }
 }
--- src/main/java/org/example/bank/Card.java
+++ src/main/java/org/example/bank/Card.java
@@ -1,4 +1,34 @@
 package org.example.bank;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Card {
+    private final long CARD_NUMBER;
+    private int pinCode;
+
+    public Card(long CARD_NUMBER) {
+        this.CARD_NUMBER = CARD_NUMBER;
+        this.pinCode = generatePin();
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        if (String.valueOf(pinCode).length() == 4) this.pinCode = pinCode;
+        else System.out.println("The PIN code must contain 4 characters");
+    }
+
+    public int generatePin() {
+        Random r = new Random();
+        return Integer.valueOf(
+                r.nextInt(1,9)+""+r.nextInt(9)+""+r.nextInt(9)+""+r.nextInt(9)
+        );
+    }
+    public void showCardInfo() {
+        System.out.println(CARD_NUMBER + " " + pinCode);
+    }
 }
